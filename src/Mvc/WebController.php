24e536ea640924491bf9bd03c5301c7ad25ccd8d
wechan <a421083@qq.com>
2018-07-10 17:28:13 +0800

    登录中心  是否登录和添加指定模板

--- src/Mvc/WebController.php
+++ src/Mvc/WebController.php
@@ -18,4 +18,76 @@ namespace Shadon\Mvc;
  */
 class WebController extends Controller
 {
+    /**
+     * @var \Eelly\DTO\UserDTO
+     */
+    protected $user;
+
+    public function onConstruct(): void
+    {
+
+    }
+    
+    public function isLogin(): void
+    {
+        // add cache service
+        $this->di->setShared('cache', function () {
+            $config = $this->getConfig()->cache->toArray();
+            $frontend = $this->get($config['frontend'], [$config['options'][$config['frontend']]]);
+
+            return $this->get($config['backend'], [$frontend, $config['options'][$config['backend']]]);
+        });
+
+        $options = $this->di->getConfig()->oauth2Client->eelly->toArray();
+        $eellyClient = \Eelly\SDK\EellyClient::initialize($options, $this->di->getCache());
+        
+        /* @var \League\OAuth2\Client\Token\AccessToken $accessToken */
+        $accessToken = $this->session->get('accessToken');
+
+        if (isset($accessToken) && $accessToken->getRefreshToken()) {
+            
+            // token 过期
+            if ($accessToken->hasExpired()) {
+                try {
+                    $accessToken = $eellyClient->getSdkClient()->getProvider()->getAccessToken(
+                        'refresh_token',
+                        ['refresh_token' => $accessToken->getRefreshToken()]
+                    );
+                } catch (IdentityProviderException $e) {
+                    $this->session->destroy();
+
+                    return;
+                }
+                $this->session->set('accessToken', $accessToken);
+            }
+            
+            $eellyClient->getSdkClient()->setAccessToken($accessToken);
+            
+            $user = new User();
+
+            try {
+                $this->view->user = $this->user = $user->getInfo();
+            } catch (LogicException $e) {
+                $this->session->set('accessToken', null);
+                $this->response->redirect('/login/account')->send();
+            }
+        } else{
+            $this->response->redirect('/login/account')->send();
+        }
+    }
+    
+    /**
+     * display other template.
+     *
+     * @param string $controller template name
+     * @param string $action     action name
+     */
+    public function sendTemplateRender($controller, $action): void
+    {
+        $this->view->render(
+            $controller,
+            $action
+        );
+        $this->response->setContent($this->view->getContent());
+    }
 }
